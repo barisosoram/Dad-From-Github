c848a8fb9aba27984d4a73d75293c5d163b39cdc
barisosoram <bsora1@live.maryville.edu>
2020-08-24 21:31:23 -0700

    init

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
---python
+import requests
+
+response=request.get(
+    httpd://icanhazdadjoke.com/',
+    headers={'Accept':'application/json'}
+    )
+    
+ print('Your dad joke:{0}'.format(response.json()['joke']))
+ 
+ input("Press ENTER to continue")
