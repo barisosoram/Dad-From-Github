8ad80442bf358352af5dd7f1bd7f6141e622bed4
barisosoram <bsora1@live.maryville.edu>
2020-08-26 07:14:02 -0700

    update

--- Main.py
+++ Main.py
@@ -1,10 +1,5 @@
 import requests
 
-response=request.get(
-    httpd://icanhazdadjoke.com/',
-    headers={'Accept':'application/json'}
-    )
-    
- print('Your dad joke:{0}'.format(response.json()['joke']))
- 
- input("Press ENTER to continue")
+response = requests.get('https://httpbin.org/ip')
+
+print('Your IP is {0}'.format(response.json()['origin']))
